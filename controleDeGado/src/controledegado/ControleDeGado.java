8b133919724399ffdc02065f3fa80eec613accbe
gislainy <>
2016-06-17 23:50:32 -0300

    Criando o projeto

---
+++ controleDeGado/src/controledegado/ControleDeGado.java
@@ -0,0 +1,21 @@
+t/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controledegado;
+
+/**
+ *
+ * @author gislainy
+ */
+public class ControleDeGado {
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    // TODO code application logic here
+  }
+  
+}
