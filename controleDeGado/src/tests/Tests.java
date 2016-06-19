7ee45b6ea88ffba2992e227bdc952e57113897cb
User <User@not>
2016-06-19 17:24:15 -0300

    teste de commit

--- controleDeGado/src/tests/Tests.java
+++ controleDeGado/src/tests/Tests.java
@@ -2,12 +2,12 @@ package tests;
 
 import db.schemas.Data;
 public class Tests {
-  public static void main(String[] args){
+  public static void main(String[] args){    
     Data dataNascimento = new Data(19,05,2015);
-    soa.Matriz vaca = new soa.Matriz();
-    vaca.adicionar(dataNascimento, 1);
-    System.out.println(vaca);
-    System.out.println(vaca);
-//    System.out.println(dataNascimento.calcularIdadeEmMeses());
+    //soa.Matriz vaca = new soa.Matriz();
+    //vaca.adicionar(dataNascimento, 1);
+    //System.out.println(vaca);
+    //System.out.println(vaca);
+    System.out.println(dataNascimento.calcularIdadeEmMeses());
   }
 }
