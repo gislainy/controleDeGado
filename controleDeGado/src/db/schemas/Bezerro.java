188e0d44e962fac71a41195d32d9774bc8de6e7b
User <User@not>
2016-06-30 20:40:11 -0300

    teste de commit 1

--- controleDeGado/src/db/schemas/Bezerro.java
+++ controleDeGado/src/db/schemas/Bezerro.java
@@ -1,6 +1,5 @@
 package db.schemas;
 
-
 public abstract class Bezerro {
   private Data idade;
   
--- controleDeGado/src/db/schemas/Touro.java
+++ controleDeGado/src/db/schemas/Touro.java
@@ -6,4 +6,35 @@ public abstract class Touro {
   private Data idade;
   private int status;
   private ArrayList<Bezerro> filhosList = new ArrayList<Bezerro>();
+  
+  Touro (Data idade, int status, ArrayList<Bezerro> filhosList){
+    this.idade = idade;
+    this.status = status;
+    this.filhosList = filhosList;
+  }
+  
+  public void setIdade(Data idade){
+    this.idade = idade;
+  }
+  
+  public Data getIdade (){
+    return idade;
+  }
+  
+  public void setStatus (int status){
+    this.status = status;
+  }
+  
+  public int getStatus (){
+    return status;
+  }
+  
+  public void setFilhosList (ArrayList<Bezerro> filhosList){
+    this.filhosList = filhosList;
+  }
+  
+  public ArrayList<Bezerro> getFilhosList (){
+    return filhosList;
+  }
+  
 }
